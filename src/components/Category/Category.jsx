3e5f308948e6be3436a5137e43866aa8784506f7
Aksh-007 <akshaybarapatre54@gmail.com>
2023-05-02 14:24:26 +0530

    Added category section

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,13 +1,19 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import "./Category.scss";
+import Products from '../Products/Products'
 
-const Category = () =>{
-    const {id} = useParams()
-    return(
-        <div>
-            <h1>Category id : {id}</h1>
+const Category = () => {
+//   const { id } = useParams();
+  return (
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="title">
+            Category Title
         </div>
-    )
-}
+        <Products productTitle={true}/>
+      </div>
+    </div>
+  );
+};
 
 export default Category;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,21 @@
 import "./Products.scss";
-import Product from './Product/Product'
-const Products = () => {
-    return (
-        <div className="products-container">
-            <div className="section-heading">Section Heading</div>
-            <div className="products">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
-        </div>
-    );
+import Product from "./Product/Product";
+const Products = ({ productTitle }) => {
+  return (
+    <div className="products-container">
+      {!productTitle && <div className="section-heading">Section Heading</div>}
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
