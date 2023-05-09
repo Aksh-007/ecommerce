ee83c7d8e7e388e3c2b7333e1f62d7051273d7a7
Aksh-007 <akshaybarapatre54@gmail.com>
2023-05-09 12:10:51 +0530

    single prduct components added

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,12 +1,21 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import "./SingleProduct.scss";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
 const SingleProduct = () => {
-    const {id} = useParams()
-    return (
-        <div>
-            <h1>Single Product id : {id}</h1>
-        </div>
-    );
+  // const {id} = useParams()
+  return (
+    <div className="single-product-main">
+      <h1>Single Product id </h1>
+    </div>
+  );
 };
 
 export default SingleProduct;
